a83a1c3042d293c8397ae67a8cb517af79638cdd
lilin <lilin@f-road.com.cn>
2019-09-29 12:30:41 +0800

    first commit

---
+++ src/test/java/com/lilin/interactiveserver/bridging/BridgingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lilin.interactiveserver.bridging;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BridgingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
